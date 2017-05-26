187d28ca798f3e07cc414bce25d1a2b062107535
futureLee1 <https://github.com/futureLee1/myHome.git>
2017-05-26 17:15:22 +0900

    change code

---
+++ src/android/Function.java
@@ -0,0 +1,131 @@
+package com.example.hello;
+
+import java.io.IOException;
+
+import org.apache.commons.lang.StringUtils;
+
+import android.nfc.tech.IsoDep;
+
+public class Function {
+	private static String MutualauthKey = "";
+	private static String SessionKey = "";
+
+	// Apdu 응답코드를 이용해 카드와 통신 수행
+	public static int Apdu(IsoDep iso, String strCmd, String[] strResponse, String[] strErrMsg) {
+		strResponse[0] = "";
+		strErrMsg[0] = "";
+		byte[] byteApdu = null;
+		byte[] result = null;
+
+		try {
+			byteApdu = util.conversion.hexStringToByteArray(strCmd);
+			iso.setTimeout(5000);
+			result = iso.transceive(byteApdu);
+		} catch (IOException e) {
+			e.printStackTrace();
+			strErrMsg[0] = e.toString();
+			return -1;
+		}
+
+		strResponse[0] = util.conversion.toHexString(result);
+
+		if (strResponse[0].equals("")) {
+			strErrMsg[0] = "Apdu Response data is null";
+			return -1;
+		}
+
+		if (!strResponse[0].substring(strResponse[0].length() - 4,
+				strResponse[0].length()).equals("9000")) {
+			return -1;
+		}
+
+		System.gc();
+		System.runFinalization();
+
+		return 0;
+	}
+
+	// 카드의 파일 검색 수행
+	public static int SelectFile(IsoDep iso, String[] strResponse, String[] strErrMsg) {
+		int res = 0;
+		String strCmd = "";
+
+		strCmd = "00A4040007D4004F54501010";
+		res = Apdu(iso, strCmd, strResponse, strErrMsg);
+		if (res < 0) {
+			return -1;
+		}
+
+		return 0;
+	}
+
+	// 카드와 인증 수행
+	public static int MutualAuthentication(IsoDep iso, String[] strResponse, String[] strErrMsg, String R1R2) {
+		int res = 0;
+		String strCmd = "";
+		String KeyBlock = "";
+		String Padding = "8000000000000000";
+		String HR = "1122334455667788";
+		String HostCryptogram = "";
+		String MAC = "";
+
+		// Key 생성
+		KeyBlock = util.conversion.Integrity_SHA256(R1R2);
+		if (KeyBlock.equals("")) {
+			return -1;
+		}
+		MutualauthKey = KeyBlock.substring(0, 32);
+
+		// HostCryptogram 생성
+		HostCryptogram = util.conversion.AES_CBC_128_ENCRYPT(HR + Padding,
+				MutualauthKey);
+		if (HostCryptogram.equals("")) {
+			return -1;
+		}
+
+		// MAC 생성
+		MAC = util.conversion.Integrity_SHA256(HostCryptogram + HR);
+		if (KeyBlock.equals("")) {
+			return -1;
+		}
+		MAC = MAC.substring(0, 16);
+
+		strCmd = "8482000018" + HostCryptogram + MAC;
+		res = Apdu(iso, strCmd, strResponse, strErrMsg);
+		if (res < 0) {
+			return -1;
+		}
+
+		// SessionKey 생성
+		String Crytogram = strResponse[0].substring(0, 32);
+		MAC = strResponse[0].substring(32, 48);
+		res = CreateSessionKey(Crytogram, MAC);
+		if (res < 0) {
+			return -1;
+		}
+
+		return 0;
+	}
+
+	// 세션키 생성 수행
+	public static int CreateSessionKey(String Crytogram, String MAC) {
+		String HostCryptogram = util.conversion.AES_CBC_128_DECRYPT(Crytogram, MutualauthKey);
+		if (HostCryptogram.equals("")) {
+			return -1;
+		}
+
+		// MAC 검증
+		String MAC_Verify = util.conversion.Integrity_SHA256(Crytogram + HostCryptogram);
+		if (MAC_Verify.equals("")) {
+			return -1;
+		}
+		MAC_Verify = MAC_Verify.toUpperCase();
+		MAC_Verify = MAC_Verify.substring(0, 16);
+
+		SessionKey = HostCryptogram;
+
+		return 0;
+	}
+
+	
+}
--- src/android/TestPlugin.java
+++ src/android/TestPlugin.java
@@ -7,6 +7,8 @@ import org.json.JSONException;
 import android.nfc.*;
 import android.util.Log;
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
 
 public class TestPlugin extends CordovaPlugin {
 
@@ -65,13 +67,26 @@ public class TestPlugin extends CordovaPlugin {
             return true;
         } else {
             Log.d(TAG, "getNfcStatus().equals(STATUS_NFC_OK)");
-            String message = "Welcome ! ";
-            callbackContext.success(message);
+            /*String message = "Welcome ! ";
+            callbackContext.success(message);*/
+
+            createPendingIntent();
+
+
         }
 
         return true;
 
 	}
+
+    private void createPendingIntent() {
+        if (pendingIntent == null) {
+            Activity activity = getActivity();
+            Intent intent = new Intent(activity, activity.getClass());
+            intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            pendingIntent = PendingIntent.getActivity(activity, 0, intent, 0);
+        }
+    }
 
     private String getNfcStatus() {
         Log.d(TAG, "In_getNfcStatus()");
@@ -90,5 +105,159 @@ public class TestPlugin extends CordovaPlugin {
         Log.d(TAG, "In_getActivity()");
         return this.cordova.getActivity();
     }
+
+    private PendingIntent getPendingIntent() {
+        return pendingIntent;
+    }
+
+    private Intent getIntent() {
+        return getActivity().getIntent();
+    }
+
+    private void setIntent(Intent intent) {
+        getActivity().setIntent(intent);
+    }
+
+    private void startNfc() {
+        createPendingIntent(); // onResume can call startNfc before execute
+
+        getActivity().runOnUiThread(new Runnable() {
+            public void run() {
+                NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(getActivity());
+
+                if (nfcAdapter != null && !getActivity().isFinishing()) {
+                    nfcAdapter.enableForegroundDispatch(getActivity(), getPendingIntent(), null, null);
+                }
+            }
+        });
+    }
+
+    private void stopNfc() {
+        Log.d(TAG, "stopNfc");
+        getActivity().runOnUiThread(new Runnable() {
+            public void run() {
+
+                NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(getActivity());
+
+                if (nfcAdapter != null) {
+                    nfcAdapter.disableForegroundDispatch(getActivity());
+                }
+            }
+        });
+    }
+
+    /*@Override
+    public void onPause(boolean multitasking) {
+        Log.d(TAG, "onPause " + getIntent());
+        super.onPause(multitasking);
+        if (multitasking) {
+            // nfc can't run in background
+            stopNfc();            
+        }
+    }
+
+    @Override
+    public void onResume(boolean multitasking) {
+        Log.d(TAG, "onResume " + getIntent());
+        super.onResume(multitasking);
+        startNfc();
+    }*/
+
+    @Override
+    public void onPause() {
+        Log.d(TAG, "onPause " + getIntent());
+        super.onPause();
+        
+        stopNfc();
+    }
+
+    @Override
+    public void onResume() {
+        Log.d(TAG, "onResume " + getIntent());
+        startNfc();
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        Log.d(TAG, "onNewIntent " + intent);
+        super.onNewIntent(intent);
+        setIntent(intent);
+        savedIntent = intent;
+        parseMessage();
+    }
 	
+    void parseMessage() {
+        cordova.getThreadPool().execute(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(TAG, "parseMessage " + getIntent());
+                Intent intent = getIntent();
+                String action = intent.getAction();
+                Log.d(TAG, "intent_action " + action);
+                
+                if (action == null) {
+                    return;
+                }
+
+                Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+                
+                String[] TechList = tag.getTechList();
+                if(find(TechList, "android.nfc.tech.IsoDep") < 0){
+                    return;
+                }
+
+                IsoDep iso = IsoDep.get(tag);
+
+                try{
+                    if(!iso.isConnected()){
+                        iso.connect();
+                    }
+
+                    Certification(iso);
+                        
+                    if(iso.isConnected()){
+                        iso.close();
+                    }
+
+                } catch(IOException e) {
+                    Log.e("IsoDep Error", e.toString());
+                }
+
+            }
+        });
+    }
+
+    private void Certification(IsoDep iso) {
+        int res = 0;
+        String strResponse[] = new String[1];
+        String strErrMsg[] = new String[1];
+        
+        res = 0;
+        res = APDU.Function.SelectFile(iso, strResponse, strErrMsg);
+        if(res < 0){
+            readResult.setText("카드 검색 실패");
+            strResponse = null;
+            strErrMsg = null;
+            return;
+        }
+
+        Log.e("Care_Result",""+strResponse[0]);
+                    
+        if(iso.isConnected()){
+            try {
+                iso.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        Intent intent = new Intent(getBaseContext(), ViewOTP.class);
+        intent.putExtra("OTP", OTP);
+        
+        OTP = "";
+        
+        startActivity(intent);
+    }
+
+
 }
